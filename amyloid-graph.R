b540c13dbcb4dc189ec9173c266b02400c7a89e5
nataliaszulc <48363461+nataliaszulc@users.noreply.github.com>
2019-12-06 22:14:11 +0100

    version1

---
+++ amyloid-graph.R
@@ -0,0 +1,32 @@
+library(visNetwork)
+library(dplyr)
+
+label_vec <- c(red = "seeding", blue = "coaggregation", green = "acceleration", 
+               black = "slowing down")
+
+
+nodes <- read.csv("nodes.csv") %>% 
+  mutate(shape = "box")
+  
+edges <- read.csv("edges_1.csv") %>% 
+  mutate(title = paste0('<a target="_blank" href="https://doi.org/', doi, '">', doi, '</a>'),
+         label = "")
+
+net <- visNetwork(nodes, edges, main = "Cross interactions between amyloids", width = 1600, height = 900) %>%
+  
+  visEdges(arrows = "to", width = 2)  %>% 
+  visLayout(randomSeed = 1337) %>% 
+  visOptions(highlightNearest = list(enabled = TRUE, degree = 1,
+                                     labelOnly = FALSE, hover = TRUE),
+             nodesIdSelection = list(enabled = TRUE)) %>% 
+  visLegend(addEdges = data.frame(color = names(label_vec), 
+                                  label = label_vec,
+                                  width = 2),
+            width = 0.2) %>% 
+  visInteraction(zoomView = TRUE)
+
+# write.csv(nodes, file = "nodes.csv")
+# write.csv(edges, file = "edges.csv")
+
+visSave(net, file = "cross_talk.html")
+
